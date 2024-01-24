4a8175d41be3948671ef89ea722b1a6a51089c51
Alex Ward <alxwrd@googlemail.com>
2024-01-24 16:19:15 +0000

    wip

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+import Skill from './Skill.astro';
+import Section from './Section.astro';
+import Job from './Job.astro';
+import Contact from './Contact.astro';
+import Link from './Link.astro';
+
+
+export { Skill, Section, Job, Contact, Link }
